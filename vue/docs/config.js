10bf6681d1c0ccf549a129c98c6503dd791c6456
Moipha <chengfayang@henu.edu.cn>
2024-12-12 13:02:21 +0800

    build: 更新https证书

--- vue/docs/config.js
+++
@@ -1,5 +0,0 @@
-export const serverIp = "zhixiao.site:4400"
-// export const serverIp = "localhost:4400"
-// export const serverIp = '39.107.246.56:4400'
-export const protocol = "https"
-//TODO 此处在部署时需要修改
--- vue/public/config.js
+++ vue/public/config.js
@@ -1,4 +1,4 @@
-export const serverIp = "zhixiao.site:4400"
+export const serverIp = "fafafafa.site:4400"
 // export const serverIp = "localhost:4400"
 // export const serverIp = '39.107.246.56:4400'
 export const protocol = "https"
